f734f5794f6346733e204c35fc5efc943f062b95
Nishanth <rajkumarnishanth@gmail.com>
2022-03-31 18:17:10 +0530

    feat: validate email

--- user_registration_app/user_registration.py
+++ user_registration_app/user_registration.py
@@ -11,6 +11,12 @@ import sys
 
 class UserRegistration:
 
+    def __init__(self) -> None:
+        self.patterns = {
+            'name': r"^[A-Z][a-z]{2,}$",
+            'email': r"^[A-Za-z0-9]{3,}([.][A-Za-z0-9]{3,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$"
+        }
+
     def get_user_info(self) -> dict[str, str]:
         """
             Description:
@@ -25,13 +31,16 @@ class UserRegistration:
         user_info: dict[str, str] = {}
         user_info['First Name'] = input("Enter First Name: ")
         user_info['Last Name'] = input("Enter Last Name: ")
-        if self.validate_name(user_info['First Name']) == False:
+        user_info['Email'] = input("Enter Email: ")
+        if self.validate_user_info(user_info['First Name'], 'name') == False:
             raise ValueError(f"Invalid First Name: {user_info['First Name']}")
-        if self.validate_name(user_info['Last Name']) == False:
+        if self.validate_user_info(user_info['Last Name'], 'name') == False:
             raise ValueError(f"Invalid Last Name: {user_info['Last Name']}")
+        if self.validate_user_info(user_info['Email'], 'email') == False:
+            raise ValueError(f"Invalid Email: {user_info['Email']}")
         return user_info
 
-    def validate_name(self, name: str) -> bool:
+    def validate_user_info(self, user_info: str, pattern_type: str) -> bool:
         """
             Description:
                 checks if the name of the user is valid
@@ -42,16 +51,19 @@ class UserRegistration:
             Return:
                 returns True if valid, else False
         """
-        name_pattern = r"^[A-Z][a-zA-Z]{2}[a-zA-Z]*$"
-        match = re.fullmatch(name_pattern, name)
-        if match:
-            return True
-        return False
+        pattern_type = pattern_type.casefold()
+        if pattern_type not in self.patterns.keys():
+            raise ValueError("Invalid pattern type given")
+        match = re.fullmatch(self.patterns[pattern_type], user_info)
+        if match == None:
+            return False
+        return True
 
 
 if __name__ == '__main__':
     logging.basicConfig(handlers=[logging.FileHandler(r"user_registration_app\user_reg.log", mode = 'a'), logging.StreamHandler(sys.stdout)], level=logging.INFO)
     user_reg = UserRegistration()
+    print(f"Name pattern: {user_reg.patterns['name']}")
     try:
         user_info = user_reg.get_user_info()
         logging.info(f"Valid user info:\n{user_info}")
