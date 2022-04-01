590bb745d4cb182d4b776aa1fa0ca14b34a3b231
Nishanth <rajkumarnishanth@gmail.com>
2022-04-01 13:23:13 +0530

    test the email validation using samples provided

--- user_registration_app/test_user_registration.py
+++ user_registration_app/test_user_registration.py
@@ -19,9 +19,31 @@ class UserRegistrationTest(unittest.TestCase):
         self.assertFalse(self.user_reg.validate_user_info("Ni", 'name'))
     
     def test_validate_email(self):
-        self.assertTrue(self.user_reg.validate_user_info("nish@gmail.com", 'email'))
-        self.assertFalse(self.user_reg.validate_user_info("n@gmail.com", 'email'))
-        self.assertFalse(self.user_reg.validate_user_info("nish@g.c", 'email'))
+        #Test Valid Emails
+        self.assertTrue(self.user_reg.validate_user_info('abc@yahoo.com', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc-100@yahoo.com', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc.100@yahoo.com', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc111@abc.com', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc-100@abc.net', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc.100@abc.com.au', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc@1.com', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc@gmail.com.com', 'email'))
+        self.assertTrue(self.user_reg.validate_user_info('abc+100@gmail.com', 'email'))
+        
+        #Test Invalid Emails
+        self.assertFalse(self.user_reg.validate_user_info('abc', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc@.com.my', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc123@gmail.a', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc123@.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc123@.com.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('.abc@abc.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc()*@gmail.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc@%*.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc..2002@gmail.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc.@gmail.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc@abc@gmail.com', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc@gmail.com.1a', 'email'))
+        self.assertFalse(self.user_reg.validate_user_info('abc@gmail.com.aa.au', 'email'))
     
     def test_validate_mobile(self):
         self.assertTrue(self.user_reg.validate_user_info("91 9952472949", 'mobile'))
